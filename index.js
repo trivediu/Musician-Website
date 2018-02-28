0b9ae9424846c584986a9f26690c2ba399600f63
trivediu <31199718+trivediu@users.noreply.github.com>
2018-02-27 18:26:41 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Jay Swaminarayan - Swami Shreeji
+
+/*CS 290 Final Project
+Uday Trivedi
+Javascript file for homepage (index)*/
+
+
+document.addEventListener("DOMContentLoaded", function(event){
+    initCar();
+});
+
+
+function initCar(){
+    $('.carousel').carousel();
+}
+
+
+
+
+
+
+
+
+
+
+
